70ce061573804ec5cf34a28ac86c677cc56512da
Nauroze Hoath <naurozehoath@gmail.com>
2022-03-08 21:46:40 -0500

    "Try out a simple web client"

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,55 @@
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/beast.hpp>
+
 #include <iostream>
-#include <boost/version.hpp>
+
+using tcp = boost::asio::ip::tcp;
+using namespace boost::beast;
+void OnConnect(boost::system::error_code ec)
+{
+    std::cerr << (ec ? "Error: " : "OK")
+            << (ec ? ec.message() : "")
+            << std::endl;
+}
 
 int main()
 {
-    std::cout << "Boost version " 
-              << BOOST_VERSION / 100000 << "."
-              << BOOST_VERSION / 100 % 1000 << "."
-              << BOOST_VERSION % 100
-              << std::endl; 
+    // I/O context object.
+    boost::asio::io_context ioc {};
+
+    // To store the error value of the eventual tcp connection
+    boost::system::error_code ec {};
+    tcp::resolver resolver(ioc);
+    auto resolverIt = resolver.resolve("ltnm.learncppthroughprojects.com", "80", ec);
+
+    if (ec) {
+        std::cerr << "Error: " << ec.message() << std::endl;
+        return -1;
+    } else {
+        std::cout << "OK" << std::endl;
+    }
+    
+    // TCP I/O object
+    tcp::socket socket(ioc);
+    socket.connect(*resolverIt, ec);
+    websocket::stream<tcp_stream> ws(std::move(socket));
+    ws.handshake("ltnm.learncppthroughprojects.com", "/echo", ec);
+
+    if (ec) {
+        std::cerr << "Error: " << ec.message() << std::endl;
+        return -1;
+    } else {
+        std::cout << "OK" << std::endl;
+    }
+
+    const char* msg = "Hello You";
+    boost::asio::const_buffer writemessage(msg, strlen(msg));
+    ws.write(writemessage, ec);
+
+    boost::beast::flat_buffer readmessage;
+    ws.read(readmessage);
+    std::cout << "Message echoed is: " << boost::beast::make_printable(readmessage.data());
+    
+    ioc.run();
 }
