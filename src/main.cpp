71d2927fc28dfc5f510cd3258f7c87bd082510ec
Nauroze Hoath <naurozehoath@gmail.com>
2022-03-12 16:40:53 -0500

    "Async client"

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,63 @@
 using tcp = boost::asio::ip::tcp;
 using namespace boost::beast;
 
+void OnRead(const boost::system::error_code& ec, std::size_t size )
+{
+    std::cerr << (ec ? "OnRead Error: " : "OnRead OK")
+    << (ec ? ec.message() : "")
+    << std::endl;
+}
+
+void OnSend(websocket::stream<tcp_stream> &ws,
+            boost::beast::flat_buffer& rBuffer,
+            const boost::system::error_code& ec 
+            )
+{
+    std::cerr << (ec ? "OnSend Error: " : "OnSend OK")
+    << (ec ? ec.message() : "")
+    << std::endl;
+
+    ws.async_read(rBuffer, OnRead);
+}
+
+void OnHandshake(websocket::stream<tcp_stream> &ws,
+                 const boost::asio::const_buffer& wBuffer,
+                 boost::beast::flat_buffer& rBuffer,
+                 const boost::system::error_code& ec )
+{
+        std::cerr << (ec ? "OnHandShake Error: " : "OnHandShake OK")
+        << (ec ? ec.message() : "")
+        << std::endl;
+
+        ws.async_write(wBuffer, 
+            [&ws, &rBuffer](auto ec, std::size_t size)
+            {
+                OnSend(ws, rBuffer, ec);
+            }
+        );
+}
+
+void OnConnect(websocket::stream<tcp_stream> &ws, 
+               const std::string& url,
+               const std::string& endpoint,
+               const boost::asio::const_buffer& wBuffer,
+               boost::beast::flat_buffer& rBuffer,
+               const boost::system::error_code& ec 
+            )
+{
+    std::cerr << (ec ? "OnConnect Error: " : "OnConnect OK")
+    << (ec ? ec.message() : "")
+    << std::endl;
+    
+    // async handshake and lambda callback
+    ws.async_handshake(url, endpoint, 
+        [&ws, &wBuffer, &rBuffer](auto ec)
+        {
+            OnHandshake(ws, wBuffer, rBuffer, ec);
+        }
+    );
+}
+
 int main()
 {
     // I/O context object.
@@ -16,8 +73,12 @@ int main()
     boost::system::error_code ec {};
     tcp::resolver resolver(ioc);
 
+    const std::string url = "ltnm.learncppthroughprojects.com";
+    const std::string endpoint = "/echo";
+    const std::string port = "80";
+    
     // Resolve URL
-    auto resolverIt = resolver.resolve("ltnm.learncppthroughprojects.com", "80", ec);
+    auto resolverIt = resolver.resolve(url, port, ec);
 
     if (ec) {
         std::cerr << "Error: " << ec.message() << std::endl;
@@ -28,9 +89,6 @@ int main()
     
     // TCP I/O object
     tcp::socket socket(ioc);
-
-    // connect to resolved endpoint
-    socket.connect(*resolverIt, ec);
     
     // message
     const char* msg = "Hello You";
@@ -38,23 +96,23 @@ int main()
     // create websocket
     websocket::stream<tcp_stream> ws(std::move(socket));
 
-    // async handshake and lambda callback
-    ws.async_handshake("ltnm.learncppthroughprojects.com", "/echo", 
-        [&msg,&ws](boost::system::error_code ec)
-        {
-            std::cerr << (ec ? "OnHandShake Error: " : "OnHandShake OK")
-            << (ec ? ec.message() : "")
-            << std::endl;
+    // create write buffer
+    boost::asio::const_buffer wBuffer(msg, strlen(msg));
 
-            boost::asio::const_buffer writemessage(msg, strlen(msg));
-            ws.write(writemessage, ec);
+    // create read buffer
+    boost::beast::flat_buffer rBuffer ;
 
-            boost::beast::flat_buffer readmessage;
-            ws.read(readmessage);
-            std::cout << "Message echoed is: " << boost::beast::make_printable(readmessage.data());
-        }
-    );
-    
+    // connect to resolved endpoint
+    ws.next_layer().async_connect(*resolverIt,
+                            [&url, &endpoint, &ws, &wBuffer, &rBuffer](auto ec)
+                            {
+                                OnConnect(ws, url, endpoint, wBuffer, rBuffer, ec);
+                            }
+                        );
+
     // Run all context work and close
     ioc.run();
+
+    // Print the echoed message
+    std::cout << boost::beast::buffers_to_string(rBuffer.data());
 }
