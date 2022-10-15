6d7730d242c59334b7c752d10005e7469841d7fe
Nauroze <naurozehoath@gmail.com>
2022-10-15 18:17:17 -0400

    Add Transport Network

---
+++ src/transport-network.cpp
@@ -0,0 +1,22 @@
+#include "transport-network.h"
+
+namespace NetworkMonitor {
+
+TransportNetwork::TransportNetwork() = default;
+
+TransportNetwork::~TransportNetwork() = default;
+
+TransportNetwork::TransportNetwork(
+    const TransportNetwork& copied
+)
+{
+
+}
+
+TransportNetwork::TransportNetwork(
+    TransportNetwork&& moved
+)
+{
+    
+}
+} // NetworkMonitor
